c03e35c7e22656cf620099781b0af4577882aa90
sbxb <sbxb@example.com>
2021-12-24 16:25:35 +0300

    Add tests for DefaultHandler and MapStorage

---
+++ internal/app/storage/storage_test.go
@@ -0,0 +1,36 @@
+package storage_test
+
+import (
+	"testing"
+
+	"github.com/sbxb/shorty/internal/app/storage"
+)
+
+func TestMemoryStore_Add_then_Get(t *testing.T) {
+	urls := []string{
+		"http://example.com",
+		"http://example.org",
+		"http://local.test",
+	}
+
+	store := storage.NewMapStorage()
+
+	for _, url := range urls {
+		id := store.Add(url)
+		urlReturned, err := store.Get(id)
+		if err != nil {
+			t.Errorf("want [%s] but failed to get one", url)
+		} else if urlReturned != url {
+			t.Errorf("got [%s], want [%s]", urlReturned, url)
+		}
+	}
+}
+
+func TestMemoryStore_Get_Nonexistent(t *testing.T) {
+	store := storage.NewMapStorage()
+	id := "nonexistent_id"
+	urlReturned, err := store.Get(id)
+	if err == nil {
+		t.Errorf("got [%s] which is not supposed to exist in storage", urlReturned)
+	}
+}
