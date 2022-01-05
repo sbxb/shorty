61d892acb503055a3f5d241b202180d15b85ea0b
sbxb <sbxb@example.com>
2022-01-05 17:13:44 +0300

    Review: make MapStorage thread-safe

--- internal/app/storage/mapstorage.go
+++ internal/app/storage/mapstorage.go
@@ -3,31 +3,37 @@ package storage
 import (
 	"fmt"
 	"strconv"
+	"sync"
 )
 
 // MapStorage defines a simple in-memory storage implemented as a wrapper aroung Go map
-// FIXME not thread-safe at the moment
 type MapStorage struct {
-	m map[string]string
+	data map[string]string
+	mu   sync.RWMutex
 }
 
 // MapStorage implements Storage interface
 var _ Storage = (*MapStorage)(nil)
 
 func NewMapStorage() *MapStorage {
-	m := make(map[string]string)
-	return &MapStorage{m}
+	d := make(map[string]string)
+	return &MapStorage{data: d}
 }
 
 func (st *MapStorage) AddURL(url string) (id string) {
 	// TODO There should be more sophisticated algorithm for calculating URL's short id
-	id = strconv.Itoa(len(st.m) + 1)
-	st.m[id] = url
+	id = strconv.Itoa(len(st.data) + 1)
+	st.mu.Lock()
+	st.data[id] = url
+	st.mu.Unlock()
 	return id
 }
 
 func (st *MapStorage) GetURL(id string) (string, error) {
-	if url, ok := st.m[id]; ok {
+	st.mu.RLock()
+	url, ok := st.data[id]
+	st.mu.RUnlock()
+	if ok {
 		return url, nil
 	}
 	return "", fmt.Errorf("id %s not found", id)
