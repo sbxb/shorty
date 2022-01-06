a61393d0e1591368aa33c30b1bc128e40e7c4abe
sbxb <sbxb@example.com>
2022-01-06 18:24:33 +0300

    Review: remove pointer receivers in favour of value receivers in config methods

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -2,7 +2,7 @@ package config
 
 import "fmt"
 
-// Very basic configuration, to be modified, nested and so on
+// Config contains application settings
 type Config struct {
 	Proto string // should be Scheme according to RFC 3986, but not on my watch
 	Host  string
@@ -17,11 +17,11 @@ var DefaultConfig = Config{
 }
 
 // FullServerName returns "hostname:port", e.g. "localhost:8080"
-func (c *Config) FullServerName() string {
+func (c Config) FullServerName() string {
 	return fmt.Sprintf("%s:%s", c.Host, c.Port)
 }
 
 // FullServerURL returns "scheme://host:port/", e.g. "http://localhost:8080/"
-func (c *Config) FullServerURL() string {
+func (c Config) FullServerURL() string {
 	return fmt.Sprintf("%s://%s:%s/", c.Proto, c.Host, c.Port)
 }
