6aa4ec6bc13704c6a45875561dc059b7f07be8be
sbxb <sbxb@example.com>
2022-01-21 20:41:14 +0300

    Review: Open() is not a part of Storage API any more

--- internal/app/storage/interface.go
+++ internal/app/storage/interface.go
@@ -2,7 +2,6 @@ package storage
 
 // Storage provides API for writing/reading URLs to/from a data store
 type Storage interface {
-	Open(credentials string) error
 	AddURL(url string, id string) error
 	GetURL(id string) (string, error)
 	Close()
