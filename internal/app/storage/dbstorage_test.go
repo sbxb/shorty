4b80f163eca79a48e14f691b5c63388faf5eacb9
sbxb <sbxb@example.com>
2022-02-04 16:39:05 +0300

    fix: comment out local db tests, they are not supposed to be run by github.com autotests

--- internal/app/storage/dbstorage_test.go
+++ internal/app/storage/dbstorage_test.go
@@ -1,95 +1,95 @@
 package storage_test
 
-import (
-	"context"
-	"testing"
+// import (
+// 	"context"
+// 	"testing"
 
-	"github.com/sbxb/shorty/internal/app/storage"
-	"github.com/sbxb/shorty/internal/app/url"
+// 	"github.com/sbxb/shorty/internal/app/storage"
+// 	"github.com/sbxb/shorty/internal/app/url"
 
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-)
+// 	"github.com/stretchr/testify/assert"
+// 	"github.com/stretchr/testify/require"
+// )
 
-const dsn = "postgres://shorty:shorty@localhost/shortytest"
+// const dsn = "postgres://shorty:shorty@localhost/shortytest"
 
-func TestDBStorage_AddBatch(t *testing.T) {
-	batch := []url.BatchURLEntry{
-		{
-			CorrelationID: "123",
-			OriginalURL:   "http://example.com",
-			ShortURL:      "/5agFZWrIb6Ej21QvYUNBL3",
-		},
-		{
-			CorrelationID: "456",
-			OriginalURL:   "http://example.org",
-			ShortURL:      "/6EH6vwAy9dOyyNbopTS6M4",
-		},
-	}
+// func TestDBStorage_AddBatch(t *testing.T) {
+// 	batch := []url.BatchURLEntry{
+// 		{
+// 			CorrelationID: "123",
+// 			OriginalURL:   "http://example.com",
+// 			ShortURL:      "/5agFZWrIb6Ej21QvYUNBL3",
+// 		},
+// 		{
+// 			CorrelationID: "456",
+// 			OriginalURL:   "http://example.org",
+// 			ShortURL:      "/6EH6vwAy9dOyyNbopTS6M4",
+// 		},
+// 	}
 
-	store, err := storage.NewDBStorage(dsn)
-	require.NoError(t, err)
-	_ = store.Truncate()
+// 	store, err := storage.NewDBStorage(dsn)
+// 	require.NoError(t, err)
+// 	_ = store.Truncate()
 
-	err = store.AddBatchURL(context.Background(), batch, "")
-	require.NoError(t, err)
-}
+// 	err = store.AddBatchURL(context.Background(), batch, "")
+// 	require.NoError(t, err)
+// }
 
-func TestDBStorage_Add_then_Get(t *testing.T) {
-	entries := []url.URLEntry{
-		{
-			ShortURL:    "5agFZWrIb6Ej21QvYUNBL3",
-			OriginalURL: "http://example.com",
-		},
-		{
-			ShortURL:    "6EH6vwAy9dOyyNbopTS6M4",
-			OriginalURL: "http://example.org",
-		},
-	}
+// func TestDBStorage_Add_then_Get(t *testing.T) {
+// 	entries := []url.URLEntry{
+// 		{
+// 			ShortURL:    "5agFZWrIb6Ej21QvYUNBL3",
+// 			OriginalURL: "http://example.com",
+// 		},
+// 		{
+// 			ShortURL:    "6EH6vwAy9dOyyNbopTS6M4",
+// 			OriginalURL: "http://example.org",
+// 		},
+// 	}
 
-	store, err := storage.NewDBStorage(dsn)
-	require.NoError(t, err)
-	_ = store.Truncate()
+// 	store, err := storage.NewDBStorage(dsn)
+// 	require.NoError(t, err)
+// 	_ = store.Truncate()
 
-	for _, ue := range entries {
-		err := store.AddURL(context.Background(), ue, "")
-		require.NoError(t, err)
+// 	for _, ue := range entries {
+// 		err := store.AddURL(context.Background(), ue, "")
+// 		require.NoError(t, err)
 
-		urlReturned, err := store.GetURL(ue.ShortURL)
-		require.NoError(t, err)
+// 		urlReturned, err := store.GetURL(ue.ShortURL)
+// 		require.NoError(t, err)
 
-		assert.Equal(t, urlReturned, ue.OriginalURL)
-	}
-}
+// 		assert.Equal(t, urlReturned, ue.OriginalURL)
+// 	}
+// }
 
-func TestDBStorage_Get_Nonexistent(t *testing.T) {
-	id := "nonexistent_id"
+// func TestDBStorage_Get_Nonexistent(t *testing.T) {
+// 	id := "nonexistent_id"
 
-	store, err := storage.NewDBStorage(dsn)
-	require.NoError(t, err)
-	_ = store.Truncate()
+// 	store, err := storage.NewDBStorage(dsn)
+// 	require.NoError(t, err)
+// 	_ = store.Truncate()
 
-	urlReturned, err := store.GetURL(id)
-	require.NoError(t, err)
+// 	urlReturned, err := store.GetURL(id)
+// 	require.NoError(t, err)
 
-	assert.Empty(t, urlReturned)
-}
+// 	assert.Empty(t, urlReturned)
+// }
 
-func TestDBStorage_Add_Record_Twice(t *testing.T) {
-	ue := url.URLEntry{
-		ShortURL:    "5agFZWrIb6Ej21QvYUNBL3",
-		OriginalURL: "http://example.com",
-	}
+// func TestDBStorage_Add_Record_Twice(t *testing.T) {
+// 	ue := url.URLEntry{
+// 		ShortURL:    "5agFZWrIb6Ej21QvYUNBL3",
+// 		OriginalURL: "http://example.com",
+// 	}
 
-	store, err := storage.NewDBStorage(dsn)
-	require.NoError(t, err)
-	_ = store.Truncate()
+// 	store, err := storage.NewDBStorage(dsn)
+// 	require.NoError(t, err)
+// 	_ = store.Truncate()
 
-	ctx := context.Background()
+// 	ctx := context.Background()
 
-	_ = store.AddURL(ctx, ue, "")   // once
-	err = store.AddURL(ctx, ue, "") // twice
+// 	_ = store.AddURL(ctx, ue, "")   // once
+// 	err = store.AddURL(ctx, ue, "") // twice
 
-	var conflictError *storage.IDConflictError
-	require.ErrorAs(t, err, &conflictError)
-}
+// 	var conflictError *storage.IDConflictError
+// 	require.ErrorAs(t, err, &conflictError)
+// }
